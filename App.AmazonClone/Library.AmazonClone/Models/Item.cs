6f9e44690bae207868718fccead8012ca95f9e9e
jacob <jakeyounan16@gmail.com>
2024-05-24 18:42:14 -0400

    initial commit - closes #1

---
+++ App.AmazonClone/Library.AmazonClone/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.AmazonClone.Models
+{
+    public class Item
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public float Price { get; set; }
+        public int Id { get; set; }
+
+        public Item() { }
+
+    }
+}
